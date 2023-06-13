c991dfe54ce8e0b710b85d5967f672994e3f7ef2
Viral Patel <its_viral@outlook.com>
2023-06-13 17:13:23 +0100

    added new api to test for oracle connection

---
+++ app/config/config.py
@@ -0,0 +1,3 @@
+oracle_username = ""
+oracle_password = ""
+oracle_dsn = ""
--- app/main.py
+++ app/main.py
@@ -1,7 +1,35 @@
 from fastapi import FastAPI
+import cx_Oracle
+from config import config
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.post("/execute_query")
+async def execute_query(sql: str):
+    # Update the connection details according to your Oracle database configuration
+    connection = cx_Oracle.connect(
+        user= config.oracle_username,
+        password= config.oracle_password,
+        dsn= config.oracle_dsn
+    )
+
+    try:
+        cursor = connection.cursor()
+
+        # Execute the SQL query
+        cursor.execute(sql)
+
+        # Fetch the result
+        result = cursor.fetchall()
+
+        return {"result": result}
+
+    finally:
+        # Close the cursor and the database connection
+        cursor.close()
+        connection.close()
