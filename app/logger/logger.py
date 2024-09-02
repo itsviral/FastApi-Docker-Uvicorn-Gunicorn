abbbb0b46b291d20d5ec9129d0e48f4440d14809
Viral Patel <its_viral@outlook.com>
2024-09-02 18:03:37 +0100

    Update logger.py

--- app/logger/logger.py
+++ app/logger/logger.py
@@ -3,33 +3,33 @@ from logging.handlers import TimedRotatingFileHandler
 from uvicorn.logging import DefaultFormatter
 
 def get_logger(name):
+    # Create a logger
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
 
-    # create a formatter that specifies the log message's format
+    # Create a formatter for log messages
     formatter = DefaultFormatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
 
-    # create a StreamHandler that sends log messages to standard output
+    # Stream handler for outputting logs to the console
     stream_handler = logging.StreamHandler()
     stream_handler.setLevel(logging.DEBUG)
-
+    stream_handler.setFormatter(formatter)
     logger.addHandler(stream_handler)
 
-    # set up a TimedRotatingFileHandler that rotates every day for the main log file
+    # File handler for rotating logs every day
     file_handler = TimedRotatingFileHandler('app/logs/app.log', when='midnight', interval=1, backupCount=60)
     file_handler.suffix = '%Y%m%d'
     file_handler.setLevel(logging.DEBUG)
     file_handler.setFormatter(formatter)
-
     logger.addHandler(file_handler)
 
-    # set up a separate file handler for Uvicorn access logs
+    # Separate handler for Uvicorn access logs
     access_log_handler = TimedRotatingFileHandler('app/logs/app.log', when='midnight', interval=1, backupCount=60)
     access_log_handler.suffix = '%Y%m%d'
     access_log_handler.setLevel(logging.INFO)
     access_log_handler.setFormatter(formatter)
 
-    # capture Uvicorn access logs in the separate file
+    # Attach the handler to Uvicorn's access logger
     uvicorn_logger = logging.getLogger('uvicorn.access')
     uvicorn_logger.addHandler(access_log_handler)
 
