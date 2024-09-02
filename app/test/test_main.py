19240a29cab42d100ba7c866b457fcbd3faa01f4
Viral Patel <its_viral@outlook.com>
2024-09-02 18:04:03 +0100

    Update test_main.py

--- app/test/test_main.py
+++ app/test/test_main.py
@@ -1,11 +1,14 @@
 import requests
 import json
 
+# URL of the FastAPI endpoint
 url = 'http://127.0.0.1:8000/oracle_query/'
 
+# SQL query to be sent to the endpoint
 query = "select * from all_tables"
 data = {'sql': query}
 
+# Send the POST request to the endpoint with the SQL query
 response = requests.post(url, json=data)
 
 # Check if the response was successful
@@ -18,4 +21,5 @@ if response.status_code == 200:
 
     print(formatted_json)
 else:
+    # Print an error message if the request was not successful
     print(f"Error: {response.status_code}")
