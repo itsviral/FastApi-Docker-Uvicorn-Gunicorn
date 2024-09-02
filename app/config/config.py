c3ba2b1092830eb4ff86494b292411641b3935b2
Viral Patel <its_viral@outlook.com>
2024-09-02 18:03:12 +0100

    Update config.py

--- app/config/config.py
+++ app/config/config.py
@@ -1,5 +1,7 @@
-oracle_username = "system"
-oracle_password = "oracle"
-oracle_service_name = "FREEPDB1"
-oracle_hostname = "localhost"
-oracle_port = 1521
+import os
+
+oracle_username = os.getenv('ORACLE_USERNAME', 'system')
+oracle_password = os.getenv('ORACLE_PASSWORD', 'oracle')
+oracle_service_name = os.getenv('ORACLE_SERVICE_NAME', 'FREEPDB1')
+oracle_hostname = os.getenv('ORACLE_HOSTNAME', 'localhost')
+oracle_port = int(os.getenv('ORACLE_PORT', 1521))
